98c2c668ab39aa0a28f7a397c3918bbe8cebfb4a
Thiago Nobre <thiago.nobre2@gmail.com>
2014-07-16 17:21:01 -0300

    adding project Moqtest.Tests

---
+++ Moqtest.Tests/TestMoqModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+
+namespace Moqtest.Tests
+{
+    [TestFixture]
+    public class TestMoqModel
+    {
+        public MoqModel Model { get; set; }
+
+        public TestMoqModel()
+        {
+            this.Model = Mock.Of<MoqModel>();
+        }
+        
+        [Test]
+        public void TestDateIsNotNull()
+        {
+            Assert.NotNull(this.Model.Date);
+        }
+
+        [Test]
+        public void TestHypotenuse()
+        {
+            var a = 3;
+            var b = 4;
+            var hypo = this.Model.Hypotenuse(a, b);
+
+            Assert.AreEqual(5, hypo);
+        }
+    }
+}
