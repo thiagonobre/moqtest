d8dba291cea558ab83286f34561edbec5978f203
Thiago Nobre <thiago.nobre2@gmail.com>
2014-07-16 17:13:44 -0300

    tests ok

---
+++ MoqModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moqtest
+{
+    public class MoqModel
+    {
+        public DateTime Date { get; set; }
+
+        public MoqModel()
+        {
+            Date = DateTime.Now;
+        }
+
+        public double Hypotenuse(double a, double b)
+        {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+    }
+}
