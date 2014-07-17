f53ae35095ec8b5111a92ba81275eeef1832016b
Thiago Nobre <thiago.nobre2@gmail.com>
2014-07-17 15:40:04 -0300

    tests added

--- Moqtest.Tests/TestMoqModel.cs
+++ Moqtest.Tests/TestMoqModel.cs
@@ -32,5 +32,48 @@ namespace Moqtest.Tests
 
             Assert.AreEqual(5, hypo);
         }
+
+        [Test]
+        public void TestGreatest()
+        {
+            var a = 3.5;
+            var b = 4;
+            var greatest = this.Model.Greatest(a, b);
+
+            Assert.AreEqual(4, greatest);
+
+            a = 3.5;
+            b = -4;
+            greatest = this.Model.Greatest(a, b);
+
+            Assert.AreEqual(3.5, greatest);
+        }
+
+        [Test]
+        public void TestTimeConversion()
+        {
+            var seconds = 556;
+            var converted = this.Model.ConvertToHMS(seconds);
+
+            Assert.AreEqual("0:9:16", converted);
+
+
+            seconds = 3600;
+            converted = this.Model.ConvertToHMS(seconds);
+
+            Assert.AreEqual("1:0:0", converted);
+
+
+            seconds = 3660;
+            converted = this.Model.ConvertToHMS(seconds);
+
+            Assert.AreEqual("1:1:0", converted);
+
+
+            seconds = 3669;
+            converted = this.Model.ConvertToHMS(seconds);
+
+            Assert.AreEqual("1:1:9", converted);
+        }
     }
 }
--- Moqtest/MoqModel.cs
+++ Moqtest/MoqModel.cs
@@ -18,5 +18,22 @@ namespace Moqtest
         {
             return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
         }
+
+        public double Greatest(double a, double b)
+        {
+            return (a + b + (Math.Abs(a-b)))/2;
+        }
+
+        public string ConvertToHMS(int seconds)
+        {
+            int s, m, h;
+
+            s = seconds % 60;
+            int totalMinutes = (seconds - s) / 60;
+            m = totalMinutes % 60;
+            h = (totalMinutes - m) / 60;
+
+            return String.Format("{0}:{1}:{2}", h, m, s);
+        }
     }
 }
